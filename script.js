e150137da97c60246c4e60e87a547269ee544dd2
Don Svederus <svederus@gmail.com>
2022-05-28 22:05:42 -0600

    0.34.1 Fetching Data

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("testing");
+// Unsplash API
+const count = 10;
+const apiKey = 'u-8YGj8Wn6Yzk_I-7869IdR4JOnsx9NL1L220faTteE';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// on Load
+getPhotos();
